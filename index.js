4e08b648ab0ac8e85345ebfca3d2d2851586ec72
Schonhoffer <andrews@iqmetrix.com>
2014-10-13 14:22:59 -0700

    treat text plain as json

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.json({ type: 'text/plain*' }))
 
 var recorded_requests = [];
 
